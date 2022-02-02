288add2ed3c80c3e699301f0c5a4425a1117eb23
Genne23v <59734889+Genne23v@users.noreply.github.com>
2022-02-02 00:01:47 -0500

    random board chat

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using FunChat.Models;
+using FunChat.App_Start;
+
+namespace FunChat
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(validateInterval: TimeSpan.FromMinutes(30),                    regenerateIdentity: (manager, user) => user.GenerateIdentityAsync(manager))
+                }
+            });
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+        }
+    }
+}
---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FunChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void JoinRoom(string chatroom)
+        {
+            Groups.Add(Context.ConnectionId, chatroom);
+        }
+        public void SendMessage(string chatroom, string message, int x, int y)
+        {
+            var username = Context.User.Identity.Name; 
+            Clients.Group(chatroom).onNewMessage(username, message, x, y);
+        }
+
+        public void SendPrivateMessage(string toUsername, string message)
+        {
+            var fromUsername = Context.User.Identity.Name;
+            Clients.Group("users/" + toUsername).onNewPrivateMessage(fromUsername, message);
+        }
+
+        public override Task OnConnected()
+        {
+            var username = Context.User.Identity.Name;
+            if (!string.IsNullOrEmpty(username))
+            {
+                Groups.Add(Context.ConnectionId, "users/" + username);
+            }
+            return base.OnConnected();
+        }
+    }
+}
